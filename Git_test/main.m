fb4adf2c7ee370a746aad5e2f2c562f6552028f0
hw3010678 <37547243@qq.com>
2015-10-22 22:49:11 +0800

    Initial Commit

---
+++ Git_test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Git_test
+//
+//  Created by 侯炜 on 15/10/22.
+//  Copyright © 2015年 hw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
