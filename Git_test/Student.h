2d4e2df638d1edd8ca9cf814bab373626975e233
hw3010678 <37547243@qq.com>
2015-10-22 23:02:55 +0800

    add student class

---
+++ Git_test/Student.h
@@ -0,0 +1,18 @@
+//
+//  Student.h
+//  Git_test
+//
+//  Created by 侯炜 on 15/10/22.
+//  Copyright © 2015年 hw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Student : NSObject{
+    
+    NSString *_name;
+    NSInteger old;
+}
+- (void) showName;
+
+@end
---
+++ Git_test/Student.m
@@ -0,0 +1,17 @@
+//
+//  Student.m
+//  Git_test
+//
+//  Created by 侯炜 on 15/10/22.
+//  Copyright © 2015年 hw. All rights reserved.
+//
+
+#import "Student.h"
+
+@implementation Student
+
+- (void) showName{
+    NSLog(@"%@",_name);
+}
+
+@end
